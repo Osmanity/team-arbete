8822044f50c17a9598242c6f97c5d7ab80dbe5a7
Ibrahim Osman <mr.ibrahim@hotmail.se>
2024-12-06 13:21:23 +0100

    Navbar available in all pages

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,11 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import About from "./Pages/About.jsx";
 import Calculator from "./Pages/Calculator.jsx";
 import Todos from "./Pages/Todos.jsx";
-
+import Navbar from "./components/Navbar.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+    <Navbar />
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/home" element={<App />} />
